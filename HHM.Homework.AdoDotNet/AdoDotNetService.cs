fadd2fee69e2beb716caf47d4f10bdf0131dca05
Hlaing_Moe <hhmoe2004@gmail.com>
2025-08-28 22:19:30 -0700

    feat: add code

---
+++ HHM.Homework.AdoDotNet/AdoDotNetService.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace HHM.Homework.AdoDotNet
+{
+    public class AdoDotNetService
+    {
+        SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "HHM.Homework.AdoDotNet",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+        public void Read()
+        {
+            using (SqlConnection con = new SqlConnection())
+            {
+                con.Open();
+                string queryRead = @"SELECT[StudentId]
+                              ,[StudentNo]
+                              ,[StudentName]
+                              ,[FatherName]
+                              ,[Gender]
+                              ,[DateOfBirth]
+                              ,[Address]
+                              ,[Result]
+                              ,[DeleteFlag]
+                         FROM[dbo].[Tbl_StudentResult]";
+                SqlCommand cmd = new SqlCommand(queryRead, con);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    DataRow dr = dt.Rows[i];
+                    string no = dr["StudentNo"].ToString();
+                    string name = dr["StudentName"].ToString();
+                    Console.WriteLine($"{i + 1} {no} - {name}");
+                }
+                con.Close();
+            }
+        }
+        public void Create()
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.Open();
+
+                string queryCreate = @"INSERT INTO[dbo].[Tbl_StudentResult]
+            ([StudentNo]
+           , [StudentName]
+           , [FatherName]
+           , [Gender]
+           , [DateOfBirth]
+           , [Address]
+           , [Result]
+           , [DeleteFlag])
+     VALUES
+           ('S001'
+           , 'Phyu'
+           , 'U Kaung'
+           , 'Female'
+           , 'Natmauk'
+           , 'Pass'
+           , '0'
+           
+           )";
+                SqlCommand cmd = new SqlCommand(queryCreate, connection);
+                int result = cmd.ExecuteNonQuery();
+
+                string message = result > 0 ? "Created Successful" : "failed to Create";
+                Console.WriteLine(message);
+            }
+
+        }
+        public void Update()
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.Open();
+
+                string query = @"UPDATE[dbo].[Tbl_StudentResult]
+                   SET[StudentName] = 'Hsu'
+                   WHERE StudentId = 1";
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+                int result = cmd.ExecuteNonQuery();
+
+                string message = result > 0 ? "Successful Update" : "failed Update";
+                Console.WriteLine(message);
+
+                connection.Close();
+            }
+        }
+
+        public void Delete()
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.Open();
+                string query = @"UPDATE[dbo].[Tbl_StudentResult]
+                    SET [DeleteFlag] = 1
+                    WHERE StudentId = 1";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                int result = cmd.ExecuteNonQuery();
+                string message = result > 0 ? "Deleting Successful" : "Deleting failed.";
+                Console.WriteLine(message);
+                connection.Close();
+
+
+            }
+
+
+        }
+
+    }
+}
+
--- HHM.Homework.AdoDotNet/Program.cs
+++ HHM.Homework.AdoDotNet/Program.cs
@@ -1,117 +1,19 @@
-﻿using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿// See https://aka.ms/new-console-template for more information
+using HHM.Homework.AdoDotNet;
 
-namespace HHM.Homework.AdoDotNet. ConsoleApp
-{
-    public class AdoDotNetServices
-    {
-        SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-        {
-            DataSource = ".",
-            InitialCatalog = "HHM.Homework.AdoDotNet",
-            UserID = "sa",
-            Password = "sasa@123",
-            TrustServerCertificate = true
-        };
-        public void Read()
-        {
-            #region
-            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-            sqlConnection.Open();
-           string query = @"SELECT[StudentId]
-                              ,[StudentNo]
-                              ,[StudentName]
-                              ,[FatherName]
-                              ,[Gender]
-                              ,[DateOfBirth]
-                              ,[Adderss]
-                              ,[Result]
-                              ,[DeleteFlag]
-                         FROM[dbo].[Tbl_StudentResult]";// read the date from the table
-                SqlCommand cmd = new SqlCommand(query, sqlConnection);
-            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-            DataTable dataTable = new DataTable();
-            adapter.Fill(dataTable);
-            sqlConnection.Close();
-            for (int i = 0; i < dataTable.Rows.Count; i++)
-            {
-                DataRow dataRow = dataTable.Rows[i];
-                Console.WriteLine($"{dataRow["StudentId"]} {dataRow["StudentNo"]} {dataRow["StudentName"]}");
-            }
-            #endregion
-        }
-        public void Create()
-        {
-            #region
-            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-            sqlConnection.Open(); 
-            string query = @"INSERT INTO[dbo].[Tbl_StudentResult]
-            ([StudentNo]
-           , [StudentName]
-           , [FatherName]
-           , [Gender]
-           , [DateOfBirth]
-           , [Adderss]
-           , [Result]
-           , [DeleteFlag])
-     VALUES
-           ('S001'
-           , 'Phyu'
-           , 'U Kaung'
-           , 'Female'
-           , 'Natmauk'
-           , 'Pass'
-           , '0'
-           
-           )";
-                SqlCommand cmd = new SqlCommand (query, sqlConnection);
-            int row = cmd.ExecuteNonQuery();
-            string message = row > 0 ? $"{row} rows affected." : "insterting failed";
-            Console.WriteLine(message);
-            sqlConnection.Close ();
-            #endregion
-        }
-        public void Update()
-        {
-            #region
-            SqlConnection sqlconnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-            sqlconnection.Open();
-            string query = @"UPDATE[dbo].[Tbl_StudentResult]
-                   SET[StudentName] = 'Hsu'
-                   WHERE StudentId = 1";
-            SqlCommand cmd = new SqlCommand(query, sqlconnection);
-            int row = cmd.ExecuteNonQuery();
-            string message = row > 0 ? $"{row} rows affected." : "updating failed";
-            Console.WriteLine(message);
-            sqlconnection.Close ();
-            #endregion
-        }
-        public void Delete()
-        {
-            #region
-            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-            sqlConnection.Open();
-            string query = @"UPDATE[dbo].[Tbl_StudentResult]
-                    SET [DeleteFlag] = 1
-                    WHERE StudentId = 1";
-            SqlCommand cmd = new SqlCommand(query, sqlConnection);
-            int row = cmd.ExecuteNonQuery();
-            string message = row > 0 ? $"{row} rows affecred." : "deleting failed";
-            Console.WriteLine(message);
-            sqlConnection.Close();
-            #endregion
+AdoDotNetService adoDotNetService = new AdoDotNetService();
 
+Console.WriteLine("Reading Data...");
+adoDotNetService.Read();
 
-        }
+Console.WriteLine("Adding Data");
+adoDotNetService.Create();
 
+Console.WriteLine("Updating Data");
+adoDotNetService.Update();
 
-    }
+Console.WriteLine("Deleting Data");
+adoDotNetService.Delete();
 
-}
-
+Console.WriteLine("Done");
+Console.ReadLine();
